a7e9b5f7bd22e5a0a66217ef7c1b94fd136c1776
sergeynagorny <sergey.nagornyj@techvice.org>
2021-08-24 16:18:18 +0300

    preparation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StyleDirective } from './directives/style.directive';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-  ],
+  declarations: [AppComponent, StyleDirective],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-
-export class AppModule { }
-
+export class AppModule {}
