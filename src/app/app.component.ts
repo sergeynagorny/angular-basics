064e5bb6ee09b4ea424ad285d8c1e0d44a2efb58
sergeynagorny <sergey.nagornyj@techvice.org>
2021-08-24 13:42:26 +0300

    add post

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,20 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
+
+export interface Post {
+  title: string;
+  text: string;
+  id?: number;
+}
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  posts: Post[] = [];
+
+  updatePosts(post: Post) {
+    this.posts.unshift(post);
+  }
+}
