a03e97fae122381dd4e9b6fdb3c74a6591726ca2
sergeynagorny <sergey.nagornyj@techvice.org>
2021-08-25 12:04:38 +0300

    app counter service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { AppCounterService } from './services/app-counter.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(public appCounterService: AppCounterService) {}
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppCounterService } from './services/app-counter.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [AppCounterService],
+  bootstrap: [AppComponent],
 })
-
-export class AppModule { }
-
+export class AppModule {}
