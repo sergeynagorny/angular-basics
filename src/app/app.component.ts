9993d340750fde1c574698fe939492ba1b0a21b8
sergeynagorny <sergey.nagornyj@techvice.org>
2021-08-25 11:43:13 +0300

    preparation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-
-export class AppComponent {
-  inputValue = ''
-
-  onInput(event: Event) {
-    this.inputValue = (<HTMLInputElement>event.target).value
-  }
-}
-
-
+export class AppComponent {}
