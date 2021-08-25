5b13bda27ad12d529602ab80f5e9a7c6af77455a
sergeynagorny <sergey.nagornyj@techvice.org>
2021-08-25 12:48:19 +0300

    service local visibility

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CounterComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
