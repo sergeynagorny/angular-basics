15fc2d66b3fbea924b5fd15e262ac2b6d2e47e97
sergeynagorny <sergey.nagornyj@techvice.org>
2021-08-25 09:29:55 +0300

    multBy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MultByPipe } from './pipes/mult-by.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-  ],
+  declarations: [AppComponent, MultByPipe],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-
-export class AppModule { }
-
+export class AppModule {}
