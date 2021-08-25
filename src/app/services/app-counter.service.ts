2a03f9533f8d2654bbfeeb99fb0ad34839a617fe
sergeynagorny <sergey.nagornyj@techvice.org>
2021-08-25 12:55:40 +0300

    inject service in service

--- src/app/services/app-counter.service.ts
+++ src/app/services/app-counter.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
+import { LogService } from './log.service';
 
 @Injectable({ providedIn: 'root' })
 export class AppCounterService {
   counter = 0;
 
+  constructor(private logService: LogService) {}
+
   increase() {
     this.counter++;
+    this.logService.log(this.counter);
   }
 
   decrease() {
     this.counter--;
+    this.logService.log(this.counter);
   }
 }
