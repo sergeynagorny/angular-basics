c44d930f8b62bf4f330d6a6f058700efa2e028d6
sergeynagorny <sergey.nagornyj@techvice.org>
2021-08-25 11:31:55 +0300

    async pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,12 @@
 import { Component } from '@angular/core';
 
-export interface Post {
-  title: string;
-  text: string;
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  searchType: keyof Post = 'title';
-  searchValue = '';
-  posts: Post[] = [
-    {
-      title: 'Milk',
-      text: 'Some milk',
-    },
-    {
-      title: 'Bread',
-      text: 'Some bread',
-    },
-    {
-      title: 'Beer',
-      text: 'Some beer',
-    },
-  ];
+  p: Promise<string> = new Promise<string>((resolve) => {
+    setTimeout(() => resolve('Promise Resolved'), 4000);
+  });
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MultByPipe } from './pipes/mult-by.pipe';
 import { FormsModule } from '@angular/forms';
-import { PostsFilterPipe } from './pipes/posts-filter.pipe';
 
 @NgModule({
-  declarations: [AppComponent, MultByPipe, PostsFilterPipe],
+  declarations: [AppComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
