daafd1970b28e74f829f3c08fc2b94630b56fcfc
sergeynagorny <sergey.nagornyj@techvice.org>
2021-08-31 11:55:26 +0300

    create form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  form: FormGroup;
+
+  constructor() {
+    this.form = new FormGroup({
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl(null, Validators.required),
+    });
+  }
+
+  submit() {
+    console.log('Form submitted: ', this.form);
+    console.log('Form data: ', { ...this.form.value });
+  }
+}
