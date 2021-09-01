ef132135eb46cf4fb1305e99761256d04cfb1c3c
sergeynagorny <sergey.nagornyj@techvice.org>
2021-09-01 17:38:12 +0300

    query params

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PostsService } from '../posts.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss'],
 })
-export class PostsComponent {
-  constructor(public postsService: PostsService) {}
+export class PostsComponent implements OnInit {
+  showIds = false;
+
+  constructor(private router: Router, private route: ActivatedRoute, public postsService: PostsService) {}
+
+  ngOnInit(): void {
+    this.showIdsByRoute();
+  }
+
+  showIdsByRoute() {
+    this.route.queryParams.subscribe((params: Params) => {
+      this.showIds = params.showIds === 'true';
+    });
+  }
+
+  showIdsByProgram() {
+    this.router.navigate(['/posts'], {
+      queryParams: {
+        showIds: !this.showIds,
+      },
+    });
+  }
 }
