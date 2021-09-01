16aa837bdbf4c6675842332f92d2370c84120b3d
sergeynagorny <sergey.nagornyj@techvice.org>
2021-09-01 17:18:42 +0300

    get element by url id

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Post, PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss'],
 })
-export class PostComponent {}
+export class PostComponent implements OnInit {
+  post: Post | undefined;
+
+  constructor(private route: ActivatedRoute, private router: Router, private postsService: PostsService) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.post = this.postsService.getById(+params.id);
+    });
+  }
+
+  loadPost() {
+    this.router.navigate(['/posts', 44]);
+  }
+}
