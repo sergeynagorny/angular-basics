b988e0c5f89e6c72cd4260a1a0c8b0d6d5537317
sergeynagorny <sergey.nagornyj@techvice.org>
2021-09-01 11:43:17 +0300

    post

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,12 +14,34 @@ export interface Todo {
 })
 export class AppComponent implements OnInit {
   todos: Todo[] = [];
+  todoTitle: string = '';
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.getTodos();
+  }
+
+  addTodo() {
+    if (!this.todoTitle.trim()) return;
+
+    const newTodo: Todo = {
+      title: this.todoTitle,
+      completed: false,
+    };
+
+    this.postTodo(newTodo);
+  }
+
+  getTodos() {
     this.http
       .get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
       .subscribe((todos) => (this.todos = todos));
+  }
+
+  postTodo(newTodo: Todo) {
+    this.http
+      .post<Todo>('https://jsonplaceholder.typicode.com/todos', newTodo)
+      .subscribe((todo) => this.todos.push(todo));
   }
 }
