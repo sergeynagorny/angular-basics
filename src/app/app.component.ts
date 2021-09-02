f4c638fbcfe31f0abd31d75289033b0b03553fe4
sergeynagorny <sergey.nagornyj@techvice.org>
2021-09-02 17:18:09 +0300

    create stream

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor() {
+    const intervalStream$ = interval(1000);
+    intervalStream$.subscribe((value) => {
+      console.log(value);
+    });
+  }
+}
