c60d97e1b165b599196961d39da9d2173f6bbb57
sergeynagorny <sergey.nagornyj@techvice.org>
2021-09-02 11:54:02 +0300

    preparation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,25 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { AboutExtraPageComponent } from './about-page/about-extra-page/about-extra-page.component';
+import { ColorDirective } from './shared/color.directive';
+import { PageNamePipe } from './shared/page-name.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    AboutPageComponent,
+    AboutExtraPageComponent,
+    ColorDirective,
+    PageNamePipe,
+  ],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
