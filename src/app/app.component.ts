0393536b278323bc997d5210bde2205b35947f7b
sergeynagorny <sergey.nagornyj@techvice.org>
2021-09-10 12:27:37 +0300

    subject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Subscription, Observable } from 'rxjs';
+import { Subscription, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,34 +8,19 @@ import { Subscription, Observable } from 'rxjs';
 })
 export class AppComponent {
   sub: Subscription;
+  stream$: Subject<number> = new Subject<number>();
+  counter = 0;
 
   constructor() {
-    const stream$ = new Observable((observer) => {
-      setTimeout(() => {
-        observer.next(1);
-      }, 1500);
-
-      setTimeout(() => {
-        observer.complete(); // завершение стрима (если до этого не было ошибок)
-      }, 1900);
-
-      setTimeout(() => {
-        observer.error('Something went wrong');
-      }, 2000);
-
-      setTimeout(() => {
-        observer.next(2);
-      }, 2500);
-    });
-
-    this.sub = stream$.subscribe(
-      (value) => console.log('Next: ', value),
-      (error) => console.log('Error: ', error),
-      () => console.log('Complete')
-    );
+    this.sub = this.stream$.subscribe((value) => console.log('Subscribe', value));
   }
 
   stopInterval() {
     this.sub.unsubscribe();
+  }
+
+  next() {
+    this.counter++;
+    this.stream$.next(this.counter);
   }
 }
